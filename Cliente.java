e3bea4df7c1a038b62afb5a51da2c7e955315a9e
Lucas Vechiez <lucas.oliveira@germinare.org.br>
2023-03-10 20:00:28 -0300

    Alterando a variavel 'nome'.

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,6 @@
 public class Cliente {
     private String tipoConta;
-    private String nome;
+    private String nome_lucas;
     private String cpf;
     private int agencia;
     private double saldo;
